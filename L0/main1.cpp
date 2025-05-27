547ad664d76398c5f18cbe6acb4a83763852b385
Leon <141495904+LeonWhiteLatex@users.noreply.github.com>
2025-05-27 20:32:14 +0300

    L0

---
+++ L0/main1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+//afhgdfhswh
+int main(){
+	std::string aboba; 
+	getline(std::cin, aboba);
+	std::cout<<aboba<<"\n";
+}
+
+/*
+bool
+char
+short
+int
+float
+long
+double
+
+unsigned
+long
+short
+const
+*/
